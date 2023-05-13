75bff9b91c4d0e91143792a9773801c38ea88063
SomeRandomGuy64 <faizan163123@gmail.com>
2023-05-13 15:20:33 +0100

    map drawn

--- src/main.lua
+++ src/main.lua
@@ -3,9 +3,21 @@ _G.love = require "love"
 function love.load()
     Object = require "src.libraries.classic"
     require "src.entity"
+    require "src.map"
     require "src.player"
 
     love.graphics.setBackgroundColor(0.3, 0.3, 0.3)
+    local map1Array = {
+        1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,1,
+        1,0,0,0,1,1,1,1,
+        1,0,0,0,0,0,0,1,
+        1,0,0,0,0,0,0,1,
+        1,0,0,0,1,0,0,1,
+        1,1,1,1,1,1,1,1
+    }
+    map1 = Map(8, 8, 64, map1Array)
 
     local p1X = 300
     local p1Y = 300
@@ -19,5 +31,6 @@ function love.update(dt)
 end
 
 function love.draw()
+    map1:draw()
     player1:draw()
 end
---
+++ src/map.lua
@@ -0,0 +1,34 @@
+Map = Object:extend()
+
+function Map:new(x, y, blockSize, arrayMap)
+    self.x = x
+    self.y = y
+    self.blockSize = blockSize
+    self.arrayMap = arrayMap
+end
+
+function Map:update(dt)
+
+end
+
+function Map:draw()
+
+    local xOffset = 0
+    local yOffset = 0
+
+    for currentY = 1, self.y + 1 do
+        for currentX = 1, self.x do
+            if self.arrayMap[(currentY - 1) * self.x + currentX] == 1 then
+                love.graphics.setColor(1, 1, 1)
+            else
+                love.graphics.setColor(0, 0, 0)        
+            end
+
+            xOffset = (currentX - 1) * self.blockSize
+            yOffset = (currentY - 1) * self.blockSize
+
+            love.graphics.rectangle("fill", xOffset, yOffset, self.blockSize, self.blockSize)
+
+        end
+    end
+end
