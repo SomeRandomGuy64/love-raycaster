1a3810874d87a65356491a597d4cd744a169f126
SomeRandomGuy64 <faizan163123@gmail.com>
2023-05-13 16:13:18 +0100

    tank controls

--- src/player.lua
+++ src/player.lua
@@ -4,6 +4,13 @@ function Player:new(x, y, playerWidth, playerHeight)
     Player.super.new(self, x, y)
     self.playerWidth = playerWidth
     self.playerHeight = playerHeight
+
+    self.angle = 100
+
+    self.speed = 10
+
+    self.deltaX = math.cos(self.angle) * self.speed
+    self.deltaY = math.cos(self.angle) * self.speed
 end
 
 function Player:update(dt)
@@ -11,24 +18,42 @@ function Player:update(dt)
 
     ---controls---
     if love.keyboard.isDown("w") then
-        self.y = self.y - (200 * dt) 
+        self.x = self.x + (self.deltaX * dt * self.speed)
+        self.y = self.y + (self.deltaY * dt * self.speed)
     end
 
     if love.keyboard.isDown("s") then
-        self.y = self.y + (200 * dt)
+        self.x = self.x - (self.deltaX * dt * self.speed)
+        self.y = self.y - (self.deltaY * dt * self.speed)
     end
 
     if love.keyboard.isDown("a") then
-        self.x = self.x - (200 * dt)
+        self.angle = self.angle - (2 * dt)
+        if self.angle < 0 then
+            self.angle = self.angle + (2 * math.pi)
+        end
+        self.deltaX = math.cos(self.angle) * self.speed
+        self.deltaY = math.sin(self.angle) * self.speed
     end
 
     if love.keyboard.isDown("d") then
-        self.x = self.x + (200 * dt)
+        self.angle = self.angle + (2 * dt)
+        if self.angle > 2 * math.pi then
+            self.angle = self.angle - (2 * math.pi)
+        end
+        self.deltaX = math.cos(self.angle) * self.speed
+        self.deltaY = math.sin(self.angle) * self.speed
     end
 end
 
 function Player:draw()
     Player.super.draw(self)
     love.graphics.setColor(1, 1, 0)
+
     love.graphics.rectangle("fill", self.x, self.y, self.playerWidth, self.playerHeight)
+
+    local lineX = self.x + (self.playerWidth / 2)
+    local lineY = self.y + (self.playerHeight / 2)
+
+    love.graphics.line(lineX, lineY, lineX + self.deltaX * 5, lineY + self.deltaY * 5)
 end
