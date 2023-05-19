e0a18b6dbb5b36f9bf905e0921831120cba08bb7
somerandomguy64 <faizan163123@gmail.com>
2023-05-19 22:24:58 +0100

    added floor and ceiling maps

--- src/map.lua
+++ src/map.lua
@@ -1,10 +1,12 @@
 Map = Object:extend()
 
-function Map:new(x, y, blockSize, arrayMap)
+function Map:new(x, y, blockSize, arrayMap, arrayFloor, arrayCeiling)
     self.x = x
     self.y = y
     self.blockSize = blockSize
     self.arrayMap = arrayMap
+    self.arrayFloor = arrayFloor
+    self.arrayCeiling = arrayCeiling
 end
 
 function Map:update(dt)
